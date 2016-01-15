f8f1f0fb4f890607e91adbd9d9b5be9fbc7e7811
Andrés Domínguez <andres_gomez@live.com.mx>
2016-01-15 12:25:23 -0700

    Sección home

---
+++ js/deepcode.js
@@ -0,0 +1,7 @@
+$(".menu-movil").click(function(){
+	$(".menu-top").toggle("slow");
+	$(".menu-top").css("margin-left",+"100%");
+});
+$(".sub-menu").click(function(){
+	$(".menu-sub").toggle("slow");
+});
