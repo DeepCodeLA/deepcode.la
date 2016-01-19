0712444c1dd78e552c28d60a39bdc74a21091227
Andrés Domínguez <andres_gomez@live.com.mx>
2016-01-18 22:49:34 -0700

    Servicios y sección 'quienes somos'

---
+++ header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Deepcode.la | Desarrollo Tecnológico</title>
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/flexboxgrid/6.3.0/flexboxgrid.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Dosis:600' rel='stylesheet' type='text/css'>
+	<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script>
+		new WOW().init();
+	</script>
+	
+</head>
+<body>
+	<header class="wow flipInX">
+		<section class="container middle-md ">
+			
+			<h1 class="col-xs-10 col-md"><span class="fa fa-code"></span> Deepcode.la</h1>
+			<div class="col-xs-2 menu-movil"><span class="fa fa-navicon"></span></div>
+			
+			<nav class="col-xs-12 col-md menu-top">
+				<ul>
+					<li class="col-xs-12 col-md">
+						<a href="index.html">
+							<div>
+								Inicio
+							</div>
+						</a>
+					</li>
+					<li class="col-xs-12 col-md">
+						<a href="#" class="sub-menu">
+							<div>
+								Servicios
+								<span class="fa fa-chevron-circle-down float-right"></span>
+							</div>
+						</a>
+						<div>
+							<ul class="menu-sub">
+								<li>
+									<a href="servicios/sistemas.html">Sistemas</a>
+								</li>
+								<li>
+									<a href="servicios/desarrollo.html">Desarrollo</a>
+								</li>
+								<li>
+									<a href="servicios/redes.html">Redes<a>
+								</li>
+								<li>
+									<a href="servicios/hardware.html">Hardware</a>
+								</li>
+								<li>
+									<a href="servicios/estrategias-">Estrategias Digitales</a>
+								</li>
+								<li>
+									<a href="servicios/#">Seguridad Informática</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li class="col-xs-12 col-md">
+						<a href="quienes-somos">
+							<div>
+								¿Quienes somos?
+							</div>
+						</a>
+					</li>
+					<li class="col-xs-12 col-md">
+						<a href="contacto">
+							<div>
+								Contacto
+							</div>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</section>
+	</header>
